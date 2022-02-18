1b0c692740108ea7b5a55a7570b983efb56f3cfe
Namit Dadlani <namitdadlani@gmail.com>
2022-02-18 00:48:44 -0400

    Updating main page

--- src/App.js
+++ src/App.js
@@ -7,8 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello! This website is under construction, please check back later.
+          Hello! This website is under construction, please check back later...
         </p>
+		<p>Till then you can check out my <a href="https://www.linkedin.com/in/namit-dadlani">Linkedin</a>!</p>
       </header>
     </div>
   );
